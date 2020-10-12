e33e8ef7fa2192e1e20f7634c02a38b06f5844eb
Luke Bower <lukebower@buddyng.com>
2020-10-12 16:22:41 +0100

    Initial Commit

---
+++ declarative-view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  declarative-view
+//
+//  Created by Buddyng on 12/10/2020.
+//  Copyright © 2020 Luke Ellis Bower. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
